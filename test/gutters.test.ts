2b22ffe6e74a9444a8d1f6b8b6a43681ec06496e
ryanluker <ryanluker@outlook.com>
2017-03-11 23:19:53 -0800

    cleanup tests and add debug logs to gitignore

--- test/gutters.test.ts
+++ test/gutters.test.ts
@@ -6,18 +6,7 @@ import * as vscode from "vscode";
 import {Gutters} from "../src/gutters";
 
 suite("Gutters Tests", () => {
-    test("Should properly setup gutters on initialize", () => {
-        let gutters = new Gutters("fakepath");
-        assert.notEqual(gutters.getIndicators(), undefined);
-        assert.equal(gutters.getWorkspacePath(), "fakepath");
-        assert.equal(gutters.getLcovPath(), "fakepath/coverage/lcov.info");
-    });
-
-    test("Should error when given a fake path to find lcov for", () => {
-        let gutters = new Gutters("fakepath2");
-
-        gutters.displayCoverageForFile("fakepath3").then(() => {
-            assert.equal(true, false);
-        });
+    test("Should setup gutters based on config values with no errors", function() {
+        const gutters = new Gutters();
     });
 });
