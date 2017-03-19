47aba8e209c33e838dc02934dd66379f0f3f69af
Ryan Luker <ryanluker@outlook.com>
2017-03-19 13:13:20 -0700

    update parse lcov d.ts

--- types/lcov-parse/index.d.ts
+++ types/lcov-parse/index.d.ts
@@ -1,25 +1,23 @@
-interface Detail {
-    hit: number,
-    line: number
-}
-
-interface Lines {
-    details: Array<Detail>
-    hit: number,
-    found: number
-}
-
-interface LcovSection {
-    branches: Object,
-    file: string,
-    functions: Object,
-    lines: Lines
-}
-
-declare function parse(file: string, cb: (err: Error, data: Array<LcovSection>) => void): void
-
 declare namespace parse {
-    function source(str: string, cb: (err: Error, data: Array<LcovSection>) => void)
+    function source(str: string, cb: (err: Error, data: Array<LcovSection>) => void): void
+
+    interface Detail {
+        hit: number,
+        line: number
+    }
+
+    interface Lines {
+        details: Array<Detail>,
+        hit: number,
+        found: number
+    }
+
+    interface LcovSection {
+        branches: Object,
+        file: string,
+        functions: Object,
+        lines: Lines
+    }
 }
 
 declare module "lcov-parse" {
