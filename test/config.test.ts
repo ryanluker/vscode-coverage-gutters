e01bc9a884014a086b0aec4556c20fd16b72cb3c
Ryan Luker <ryanluker@outlook.com>
2017-03-19 13:54:36 -0700

    setup test files

---
+++ test/config.test.ts
@@ -0,0 +1,12 @@
+"use strict";
+
+import * as assert from "assert";
+
+import * as vscode from "vscode";
+import {Config} from "../src/config";
+
+suite("Config Tests", function() {
+    test("Constructor should setup properly", function(done) {
+
+    });
+});
---
+++ test/lcov.test.ts
@@ -0,0 +1,12 @@
+"use strict";
+
+import * as assert from "assert";
+
+import * as vscode from "vscode";
+import {Lcov} from "../src/lcov";
+
+suite("Lcov Tests", function() {
+    test("Constructor should setup properly", function(done) {
+
+    });
+});
