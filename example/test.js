4c2959789c7a6d29611da7a0544f6db9a32ba87e
ryanluker <ryanluker@outlook.com>
2017-03-26 11:59:36 -0700

    update example test

--- example/test.js
+++ example/test.js
@@ -4,8 +4,13 @@ const assert   = require('assert');
 const testFunc = require('./test-coverage').test;
 
 describe('test func', function() {
-    it('should not show coverage on lines 5 and 9', function() {
+    it('should show coverage on line 8', function() {
         var num = testFunc(3);
         assert(num==="null");
+    });
+
+    it('should show coverage on line 6', function() {
+        var num = testFunc(2);
+        assert(num===false);
     });
 });
