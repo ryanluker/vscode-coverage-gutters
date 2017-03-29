74e0bc75cc5da7f3e17eb0c7184fce8081074163
ryanluker <ryanluker@outlook.com>
2017-03-28 23:43:40 -0700

    update example

--- example/test-coverage.js
+++ example/test-coverage.js
@@ -2,9 +2,14 @@ module.exports.test = function test(testNumber) {
     if(testNumber === 1) {
         return true;
     }
+    if(testNumber === 5) {
+        return "reallyReally";
+    }
+
     if(testNumber === 2) {
         return false;
     }
     if(testNumber === 3) return "notNull";
+    if(testNumber === 4) return "reallyNull";
     return "null";
 }
--- example/test.js
+++ example/test.js
@@ -5,12 +5,17 @@ const testFunc = require('./test-coverage').test;
 
 describe('test func', function() {
     it('should show coverage on line 8', function() {
-        var num = testFunc(4);
-        assert(num==="null");
+        var num = testFunc(1);
+        assert(num===true);
     });
 
     it('should show coverage on line 6', function() {
         var num = testFunc(2);
         assert(num===false);
+    });
+
+    it('should show  coverage on line 9', function() {
+        var num = testFunc(4);
+        assert(num==="reallyNull");
     });
 });
