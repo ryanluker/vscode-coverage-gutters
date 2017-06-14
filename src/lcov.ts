948fda6f0332bc542f44f1c2b5ba151aa1bf6dec
ryanluker <ryanluker@outlook.com>
2017-06-13 23:10:16 -0700

    refactor find to use glob instead of vscode find

--- src/lcov.ts
+++ src/lcov.ts
@@ -1,29 +1,35 @@
 import {IConfigStore} from "./config";
 import {InterfaceFs} from "./wrappers/fs";
+import {InterfaceGlob} from "./wrappers/glob";
 import {InterfaceVscode} from "./wrappers/vscode";
 
 export class Lcov {
     private configStore: IConfigStore;
+    private glob: InterfaceGlob;
     private vscode: InterfaceVscode;
     private fs: InterfaceFs;
 
     constructor(
         configStore: IConfigStore,
+        glob: InterfaceGlob,
         vscode: InterfaceVscode,
         fs: InterfaceFs,
     ) {
         this.configStore = configStore;
+        this.glob = glob;
         this.vscode = vscode;
         this.fs = fs;
     }
 
     public find(): Promise<string> {
-        return new Promise((resolve, reject) => {
-            this.vscode.findFiles("**/" + this.configStore.lcovFileName, "**/node_modules/**")
-                .then((uriLcov) => {
-                    if (!uriLcov || !uriLcov.length) { return reject("Could not find a lcov file!"); }
-                    if (uriLcov.length > 1) { return reject("More then one lcov file found!"); }
-                    return resolve(uriLcov[0].fsPath);
+        return new Promise<string>((resolve, reject) => {
+            this.glob.find(
+                `**/${this.configStore.lcovFileName}`,
+                { ignore: "**/node_modules/**", cwd: this.vscode.getRootPath(), realpath: true },
+                (err, files) => {
+                    if (!files || !files.length) { return reject("Could not find a lcov file!"); }
+                    if (files.length > 1) { return reject("More then one lcov file found!"); }
+                    return resolve(files[0]);
                 });
         });
     }
