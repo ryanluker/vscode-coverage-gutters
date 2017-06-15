03ce25c098de255026b923f32532d5c1e171a985
ryanluker <ryanluker@outlook.com>
2017-06-14 18:32:18 -0700

    wip console log

--- test/index.ts
+++ test/index.ts
@@ -10,6 +10,7 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
+console.log("************************************** here?");
 // tslint:disable-next-line:no-var-requires
 const testRunner = require("vscode/lib/testrunner");
 
