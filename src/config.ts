48696fb05fddd3930a9fa5d973a9866931099465
Ryan Luker <ryanluker@outlook.com>
2017-06-18 16:10:51 -0700

    show only gutters by default, add events

--- src/config.ts
+++ src/config.ts
@@ -85,6 +85,8 @@ export class Config {
         const showGutterCoverage = rootConfig.get("showGutterCoverage") as string;
         const showLineCoverage = rootConfig.get("showLineCoverage") as string;
         const showRulerCoverage = rootConfig.get("showRulerCoverage") as string;
+        this.reporter.sendEvent("config", "showLineCoverage", showLineCoverage);
+        this.reporter.sendEvent("config", "showRulerCoverage", showRulerCoverage);
 
         const fullDecoration: DecorationRenderOptions = {
             dark: {
