2a0fc9fdae51dade5225ace6c07c29aede8bd691
Ryan Luker <ryanluker@outlook.com>
2017-06-18 16:14:28 -0700

    Add more test coverage to config component (#71)
    * add config tests and cleanup setup
    
    * cleanup lints

--- test/config.test.ts
+++ test/config.test.ts
@@ -3,7 +3,48 @@ import * as vscode from "vscode";
 import {Config} from "../src/config";
 
 suite("Config Tests", function() {
-    test("Constructor should setup properly", function() {
+    const fakeVscode: any = {
+        createTextEditorDecorationType: (options) => {
+            assert.equal(Object.keys(options).length, 4);
+            return {};
+        },
 
+        executeCommand: () => {
+            return ;
+        },
+
+        getConfiguration: () => {
+            return {
+                get: () => "123",
+                test1: "test1",
+                test2: "test2",
+                test3: "test3",
+            };
+        },
+    };
+
+    const fakeContext: any = {
+        asAbsolutePath: () => {
+            return ;
+        },
+    };
+
+    const fakeReport: any = {
+        sendEvent: () => {
+            return ;
+        },
+    };
+
+    test("Constructor should setup properly", function() {
+        assert.doesNotThrow(() => {
+            const _CONFIG = new Config(fakeVscode, fakeContext, fakeReport);
+        });
+    });
+
+    test("Can get configStore after initialization", function() {
+        const config = new Config(fakeVscode, fakeContext, fakeReport);
+        const store = config.get();
+        assert.notEqual(store.altSfCompare, null);
+        assert.notEqual(store.lcovFileName, null);
     });
 });
