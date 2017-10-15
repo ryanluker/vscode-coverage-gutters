552e6b6235f0d8644641f4b009b502929876c84e
ryanluker <ryanluker@outlook.com>
2017-10-15 10:36:09 -0700

    setup python example and update deps

---
+++ example/python/README.md
@@ -0,0 +1,2 @@
+Use the command below to generate a test xml file
+```py.test --cov-report xml:cov.xml --cov```
---
+++ example/python/test_sample.py
@@ -0,0 +1,13 @@
+"""testing coverage"""
+
+# content of test_sample.py
+def func(number):
+    """test function"""
+    if number is 4:
+        return number + 1
+    else:
+        return number
+
+def test_answer():
+    """test adding"""
+    assert func(4) == 5
