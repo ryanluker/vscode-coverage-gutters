c38538488bff58bfab81575c703208968015c48c
ryanluker <ryanluker@outlook.com>
2018-04-15 20:05:53 -0700

    [#117] Cleanup integration test

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,23 +1,13 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import {activate} from "../src/extension";
 
 suite("Extension Tests", function() {
-    this.timeout(4000);
+    this.timeout(50000);
 
-    test("Should open extension @integration", () => {
-        const fakeContext: any = {
-            asAbsolutePath: () => {
-                return "123";
-            },
-            subscriptions: [],
-        };
-        activate(fakeContext);
-
-        assert.equal(
-            fakeContext.subscriptions.length,
-            6,
-            "Does not have the proper amount of functionality",
-        );
+    test("Should start extension @integration", (done) => {
+        const started = vscode.extensions.getExtension(
+            "ryanluker.vscode-coverage-gutters",
+        ).isActive;
+        assert.equal(started, true);
     });
 });
