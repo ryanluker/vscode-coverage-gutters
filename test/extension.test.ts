157223e3a8e9b63dc6dc799ecf2db18d220e6100
ryanluker <ryanluker@outlook.com>
2018-04-15 20:27:40 -0700

    [#117] Cleanup integration test return flow

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -4,7 +4,7 @@ import * as vscode from "vscode";
 suite("Extension Tests", function() {
     this.timeout(50000);
 
-    test("Should start extension @integration", (done) => {
+    test("Should start extension @integration", async () => {
         const started = vscode.extensions.getExtension(
             "ryanluker.vscode-coverage-gutters",
         ).isActive;
