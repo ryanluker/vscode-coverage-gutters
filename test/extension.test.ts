2d9142dbcb39f9f254f2e69dd27f7a30dfb8b140
Ryan Luker <ryanluker@outlook.com>
2018-11-04 10:27:57 -0800

    [#182] Add large code base integraton test for glob ignore bug

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,4 +1,5 @@
 import * as assert from "assert";
+import { exec } from "child_process";
 import * as vscode from "vscode";
 import {ICoverageLines} from "../src/coverage-system/renderer";
 
@@ -17,6 +18,41 @@ suite("Extension Tests", function() {
         const emptyLines = extension.exports.emptyLastCoverage;
         const testCoverage = await vscode.workspace.findFiles("**/test-coverage.js", "**/node_modules/**");
         const testDocument = await vscode.workspace.openTextDocument(testCoverage[0]);
+        await vscode.window.showTextDocument(testDocument);
+        await vscode.commands.executeCommand("extension.displayCoverage");
+
+        // Wait for decorations to load
+        await sleep(2000);
+
+        // Look for exact coverage on the file
+        const cachedLines: ICoverageLines = getCachedLines();
+        assert.equal(14, cachedLines.full.length);
+        assert.equal(4, cachedLines.none.length);
+        assert.equal(7, cachedLines.partial.length);
+        emptyLines();
+    });
+
+    test("Run display coverage on node test file with large code base @integration", async () => {
+        // Wait for extension to load
+        await sleep(2000);
+
+        const extension = await vscode.extensions.getExtension("ryanluker.vscode-coverage-gutters");
+        if (!extension) {
+            throw new Error("Could not load extension");
+        }
+        const getCachedLines = extension.exports.getLastCoverageLines;
+        const emptyLines = extension.exports.emptyLastCoverage;
+        const testCoverage = await vscode.workspace.findFiles("**/test-coverage.js", "**/node_modules/**");
+        const testDocument = await vscode.workspace.openTextDocument(testCoverage[0]);
+
+        // Generate more files to test for large code bases #182
+        await exec("cp -r node node2");
+        await exec("cp -r node node3");
+        await exec("cp -r node node4");
+        await exec("cp -r node node5");
+        await exec("cp -r node node6");
+        await exec("cp -r node node7");
+
         await vscode.window.showTextDocument(testDocument);
         await vscode.commands.executeCommand("extension.displayCoverage");
 
