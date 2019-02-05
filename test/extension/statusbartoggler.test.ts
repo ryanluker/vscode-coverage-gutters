bed96910f6111d6455c28f03e4a59f6f0f826be4
Felipe Caputo <felipe.caputo@gmail.com>
2019-02-05 18:08:34 -0200

    Change `statusBar.toggle` to receive the toggle state
    Fix #206

--- test/extension/statusbartoggler.test.ts
+++ test/extension/statusbartoggler.test.ts
@@ -1,13 +1,34 @@
-import {StatusBarToggler} from "../../src/extension/statusbartoggler";
+import * as assert from "assert";
+import { StatusBarToggler } from "../../src/extension/statusbartoggler";
 import { fakeConfig } from "../mocks/fakeConfig";
 
-suite("Status Bar Toggler Tests", function() {
-    test("Should toggle showStatusBarToggler command and message @unit", function() {
+suite("Status Bar Toggler Tests", () => {
+    test("Should toggle showStatusBarToggler command and message @unit", () => {
         const statusBarToggler = new StatusBarToggler(fakeConfig);
-        statusBarToggler.toggle();
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch");
     });
 
-    test("Should dispose when asked @unit", function() {
+    test("Should not toggle twice showStatusBarToggler command and message @unit", () => {
+        const statusBarToggler = new StatusBarToggler(fakeConfig);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch");
+    });
+
+    test("Should toggle showStatusBarToggler command and message back to \"Wath\" @unit", () => {
+        const statusBarToggler = new StatusBarToggler(fakeConfig);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch");
+        statusBarToggler.toggle(false);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+    });
+
+    test("Should dispose when asked @unit", () => {
         const statusBarToggler = new StatusBarToggler(fakeConfig);
         statusBarToggler.dispose();
     });
