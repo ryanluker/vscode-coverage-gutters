dbde96221d09f6fe54c9aa41a17e9b85d72f43bb
Ryan Luker <ryanluker@outlook.com>
2019-05-12 09:46:43 -0700

    [#198] Use homemade mocking solution Also un skip the coverage tests and make sure they still work!

--- test/coverage-system/coverage.test.ts
+++ test/coverage-system/coverage.test.ts
@@ -1,11 +1,23 @@
 import * as assert from "assert";
+import * as fs from "fs";
+import * as vscode from "vscode";
+
 import {Coverage} from "../../src/coverage-system/coverage";
 import {fakeConfig} from "../mocks/fakeConfig";
 
+// Original functions
+const readFile = fs.readFile;
+const showQuickPick = vscode.window.showQuickPick;
+
 suite("Coverage Tests", function() {
+    teardown(function() {
+        (fs as any).readFile = readFile;
+        (vscode as any).window.showQuickPick = showQuickPick;
+    });
+
     test("Constructor should setup properly @unit", function(done) {
         try {
-            const coverage = new Coverage(
+            new Coverage(
                 fakeConfig,
             );
             return done();
@@ -15,12 +27,14 @@ suite("Coverage Tests", function() {
         }
     });
 
-    test.skip("#load: Should reject when readFile returns an error @unit", function(done) {
+    test("#load: Should reject when readFile returns an error @unit", function(done) {
         const readFile = function(path: string, cb) {
             assert.equal(path, "pathtofile");
             const error: NodeJS.ErrnoException = new Error("could not read from fs");
             return cb(error, new Buffer(""));
         };
+        (fs as any).readFile = readFile;
+
         const coverage = new Coverage(
             fakeConfig,
         );
@@ -36,11 +50,12 @@ suite("Coverage Tests", function() {
             });
     });
 
-    test.skip("#load: Should return a data string @unit", function(done) {
+    test("#load: Should return a data string @unit", function(done) {
         const readFile = function(path: string, cb: (err: NodeJS.ErrnoException, data: Buffer) => void) {
             assert.equal(path, "pathtofile");
             return cb(undefined as any, new Buffer("lcovhere"));
         };
+        (fs as any).readFile = readFile;
 
         const coverage = new Coverage(
             fakeConfig,
@@ -56,8 +71,10 @@ suite("Coverage Tests", function() {
             });
     });
 
-    test.skip("#pickFile: Should return undefined if no item is picked @unit", function(done) {
+    test("#pickFile: Should return undefined if no item is picked @unit", function(done) {
         const showQuickPick = async () => undefined;
+        (vscode as any).window.showQuickPick = showQuickPick;
+
         const coverage = new Coverage(
             fakeConfig,
         );
