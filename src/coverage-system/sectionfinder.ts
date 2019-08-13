9f58903e20264ff3ff7820542fc7bf4480eb4141
Ryan Luker <ryanluker@outlook.com>
2019-08-12 22:41:37 -0700

    Add missing return

--- src/coverage-system/sectionfinder.ts
+++ src/coverage-system/sectionfinder.ts
@@ -115,6 +115,7 @@ export class SectionFinder {
         const workspaceFolderName = workspaceFolder.name;
         return { relativePath: editorFileRelative, workspaceFolder: workspaceFolderName};
     }
+
     /**
      * Returns true if relative section matches given editor file
      * @param sectionFileName relative section fileName
