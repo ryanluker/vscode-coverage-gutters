ee50513296e2e299c680bb68a1b43658102e813a
Ryan Luker <ryanluker@outlook.com>
2019-10-19 10:20:28 -0700

    [#225] Properly bind gutters to the callback handler

--- src/extension.ts
+++ src/extension.ts
@@ -23,23 +23,23 @@ export function activate(context: vscode.ExtensionContext) {
 
     const previewCoverageReport = vscode.commands.registerCommand(
         "extension.previewCoverageReport",
-        gutters.previewCoverageReport,
+        gutters.previewCoverageReport.bind(gutters),
     );
     const display = vscode.commands.registerCommand(
         "extension.displayCoverage",
-        gutters.displayCoverageForActiveFile,
+        gutters.displayCoverageForActiveFile.bind(gutters),
     );
     const watch = vscode.commands.registerCommand(
         "extension.watchCoverageAndVisibleEditors",
-        gutters.watchCoverageAndVisibleEditors,
+        gutters.watchCoverageAndVisibleEditors.bind(gutters),
     );
     const removeWatch = vscode.commands.registerCommand(
         "extension.removeWatch",
-        gutters.removeWatch,
+        gutters.removeWatch.bind(gutters),
     );
     const remove = vscode.commands.registerCommand(
         "extension.removeCoverage",
-        gutters.removeCoverageForActiveFile,
+        gutters.removeCoverageForActiveFile.bind(gutters),
     );
 
     context.subscriptions.push(previewCoverageReport);
