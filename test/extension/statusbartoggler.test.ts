e2e4ba805f130961d76f0b62e3a53044cf9a2fbc
Patrick Fowler <pfowler@cs.stanford.edu>
2020-07-10 20:49:22 -0700

    Adds test to cover the new loading spinner in the Status Bar

--- test/extension/statusbartoggler.test.ts
+++ test/extension/statusbartoggler.test.ts
@@ -28,6 +28,27 @@ suite("Status Bar Toggler Tests", () => {
         assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
     });
 
+    test("Should show the spinner when setting the `isLoading` status @unit", () => {
+        const statusBarToggler = new StatusBarToggler(fakeConfig);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+        statusBarToggler.setLoading(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch $(loading~spin)");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch $(loading~spin)");
+        statusBarToggler.setLoading(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch");
+    })
+
+    test("Should show progress regardless of status @unit", () => {
+        const statusBarToggler = new StatusBarToggler(fakeConfig);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch");
+        statusBarToggler.setFileProgress(0,10);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Watch (0/10)");
+        statusBarToggler.toggle(true);
+        assert.equal(statusBarToggler.statusText, "$(list-ordered) Remove Watch (0/10)");
+
+    })
+
     test("Should dispose when asked @unit", () => {
         const statusBarToggler = new StatusBarToggler(fakeConfig);
         statusBarToggler.dispose();
