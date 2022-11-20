ecdb5ecfa5e3df22a8c12fcfe9683754b75631f9
ryanluker <ryanluker@outlook.com>
2022-11-20 11:30:45 -0800

    use non-insiders for UA testing

--- test/runTest.ts
+++ test/runTest.ts
@@ -10,7 +10,7 @@ async function main() {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, "..", "..");
         const extensionTestsPath = path.resolve(__dirname, "index");
-        const vscodeExecutablePath = await downloadAndUnzipVSCode("insiders");
+        const vscodeExecutablePath = await downloadAndUnzipVSCode();
 
         // Add the dependent extension for test coverage preview functionality
         const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
