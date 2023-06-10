256943f50575404b861090a5c5680229b24a2e89
Ryan Luker <ryanluker@outlook.com>
2023-06-09 22:24:10 -0700

    use proper ts variables

--- test/runTest.ts
+++ test/runTest.ts
@@ -11,13 +11,13 @@ async function main() {
         const extensionDevelopmentPath = path.resolve(__dirname, "..", "..");
         const extensionTestsPath = path.resolve(__dirname, "index");
 
+        let vscodeExecutablePath;
         // Use win64 instead of win32 for testing Windows
         if (process.platform === 'win32') {
-          const vscodeExecutablePath = await downloadAndUnzipVSCode("win32-x64-archive");
+          vscodeExecutablePath = await downloadAndUnzipVSCode("win32-x64-archive");
         } else {
-          const vscodeExecutablePath = await downloadAndUnzipVSCode();
+          vscodeExecutablePath = await downloadAndUnzipVSCode();
         }
-
 
         // Add the dependent extension for test coverage preview functionality
         const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
