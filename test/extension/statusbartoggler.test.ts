29a8850bd5169e50b0126e0128c728c64054aff2
Matt Seddon <37993418+mattseddon@users.noreply.github.com>
2024-11-18 05:09:20 +1100

    Update text displayed on Status Bar Item (#458)
    Co-authored-by: Ryan Luker <ryanluker@outlook.com>

--- test/extension/statusbartoggler.test.ts
+++ test/extension/statusbartoggler.test.ts
@@ -9,48 +9,74 @@ suite("Status Bar Toggler Tests", () => {
     test("Should toggle showStatusBarToggler command and message @unit", () => {
         const statusBarToggler = new StatusBarToggler(stubConfig);
         statusBarToggler.toggle(true);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
     });
 
     test("Should not toggle twice showStatusBarToggler command and message @unit", () => {
         const statusBarToggler = new StatusBarToggler(stubConfig);
         statusBarToggler.toggle(true);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
         statusBarToggler.toggle(true);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
     });
 
-    test("Should toggle showStatusBarToggler command and message back to \"Watch\" @unit", () => {
+    test('Should toggle showStatusBarToggler command and message back to "Watch" @unit', () => {
         const statusBarToggler = new StatusBarToggler(stubConfig);
         statusBarToggler.toggle(true);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
         statusBarToggler.toggle(false);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) Watch");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) Watch"
+        );
     });
 
     test("Should show the spinner when setting the `isLoading` status @unit", () => {
         const statusBarToggler = new StatusBarToggler(stubConfig);
         statusBarToggler.setLoading(true);
-        expect(statusBarToggler.statusText).to.equal("$(loading~spin) Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(loading~spin) Coverage"
+        );
         statusBarToggler.toggle(true);
-        expect(statusBarToggler.statusText).to.equal("$(loading~spin) Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(loading~spin) Coverage"
+        );
         statusBarToggler.setLoading(false);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
     });
 
     test("Should show coverage when a number is set @unit", () => {
         const statusBarToggler = new StatusBarToggler(stubConfig);
         statusBarToggler.toggle(true);
         statusBarToggler.setLoading(false);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
         statusBarToggler.setCoverage(84);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) 84% Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File 84% Covered"
+        );
         statusBarToggler.setCoverage(undefined);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) No Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
         statusBarToggler.setCoverage(50);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) 50% Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File 50% Covered"
+        );
         statusBarToggler.setCoverage(0);
-        expect(statusBarToggler.statusText).to.equal("$(circle-large-outline) 0% Coverage");
+        expect(statusBarToggler.statusText).to.equal(
+            "$(circle-large-outline) File Uncovered"
+        );
     });
 
     test("Should dispose when asked @unit", () => {
