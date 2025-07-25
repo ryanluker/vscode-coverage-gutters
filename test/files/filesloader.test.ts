427d8bc3da0b541ea5ba31fc63dfa3616a1373ba
Matt Seddon <mattseddon@hotmail.com>
2025-07-25 16:18:06 +1000

    revert node example

--- test/files/filesloader.test.ts
+++ test/files/filesloader.test.ts
@@ -13,19 +13,11 @@ suite("FilesLoader Tests", () => {
     teardown(() => sinon.restore());
 
     test("loadDataFiles takes file paths and fetches their data strings @unit", async () => {
-        sinon
-            .stub(fs, "readFile")
-            .callsFake(
-                (
-                    _: number | PathLike,
-                    cb: (
-                        err: NodeJS.ErrnoException | null,
-                        data: Buffer
-                    ) => void
-                ) => {
-                    return cb(null, Buffer.from("123"));
-                }
-            );
+        sinon.stub(fs, "readFile").callsFake(
+            (_: number | PathLike, cb: (err: NodeJS.ErrnoException | null, data: Buffer) => void) => {
+                return cb(null, Buffer.from("123"));
+            },
+        );
 
         const filesLoader = new FilesLoader(stubConfig);
         const testData = new Set(["file1", "file2"]);
@@ -33,60 +25,28 @@ suite("FilesLoader Tests", () => {
 
         expect(mapData.size).to.equal(2);
         expect(mapData.get("file1")).to.equal("123");
+
     });
 
     test("findCoverageFiles returns an error if no coverage file @unit", async () => {
         stubConfig.manualCoverageFilePaths = [];
         stubConfig.coverageFileNames = ["lcov.info"];
         const filesLoader = new FilesLoader(stubConfig);
-        sinon
-            .stub(filesLoader as any, "findCoverageInWorkspace")
-            .resolves(new Map());
+        sinon.stub(filesLoader as any, "findCoverageInWorkspace").resolves(new Map());
 
-        const stubShowWarningMessage = sinon.spy(
-            vscode.window,
-            "showWarningMessage"
-        );
+        const stubShowWarningMessage = sinon.spy(vscode.window, "showWarningMessage");
 
         await filesLoader.findCoverageFiles();
-        expect(
-            stubShowWarningMessage.calledWith(
-                "Could not find a Coverage file! Searched for lcov.info"
-            )
-        );
+        expect(stubShowWarningMessage.calledWith("Could not find a Coverage file! Searched for lcov.info"));
     });
 
     test("findCoverageFiles returns manual coverage paths if set @unit", async () => {
-        const nodeUnitTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "node",
-            "unit",
-            "lcov.info"
-        );
-        const nodeIntTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "node",
-            "integration",
-            "lcov.info"
-        );
-        const rubyTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "ruby",
-            "lcov.info"
-        );
-        const coverageFiles = [nodeUnitTestFile, nodeIntTestFile, rubyTestFile];
+        const nodeTestFile = path.resolve(__dirname, "..", "..", "..", "example", "node", "lcov.info")
+        const rubyTestFile = path.resolve(__dirname, "..", "..", "..", "example", "ruby", "lcov.info")
+        const coverageFiles = [
+            nodeTestFile,
+            rubyTestFile,
+        ];
         stubConfig.manualCoverageFilePaths = coverageFiles;
         const filesLoader = new FilesLoader(stubConfig);
         const files = await filesLoader.findCoverageFiles();
@@ -94,45 +54,21 @@ suite("FilesLoader Tests", () => {
     });
 
     test("findCoverageFiles returns only manual coverage paths that exist @unit", async () => {
-        const nodeUnitTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "node",
-            "unit",
-            "lcov.info"
-        );
-
-        const unknownTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "unknown",
-            "lcov.info"
-        );
-        const rubyTestFile = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "..",
-            "example",
-            "ruby",
-            "lcov.info"
-        );
-        const coverageFiles = [nodeUnitTestFile, unknownTestFile, rubyTestFile];
+        const nodeTestFile = path.resolve(__dirname, "..", "..", "..", "example", "node", "lcov.info")
+        const unknownTestFile = path.resolve(__dirname, "..", "..", "..", "example", "unknown", "lcov.info")
+        const rubyTestFile = path.resolve(__dirname, "..", "..", "..", "example", "ruby", "lcov.info")
+        const coverageFiles = [
+            nodeTestFile,
+            unknownTestFile,
+            rubyTestFile,
+        ];
         stubConfig.manualCoverageFilePaths = coverageFiles;
         const filesLoader = new FilesLoader(stubConfig);
         const files = await filesLoader.findCoverageFiles();
-        expect(
-            new Set([
-                nodeUnitTestFile,
-                // unknownTestFile does not exist so it should not be included
-                rubyTestFile,
-            ])
-        ).to.deep.equal(files);
+        expect(new Set([
+            nodeTestFile,
+            // unknownTestFile does not exist so it should not be included
+            rubyTestFile,
+        ])).to.deep.equal(files);
     });
 });
