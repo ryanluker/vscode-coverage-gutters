4bc98250d6f944b1d51e5fd45d144a39aad1c7ec
Ryan Luker <ryanluker@outlook.com>
2026-06-25 11:55:49 -0700

    Add retries to the e2e extension tests (#518)
    * Add retries to the e2e extension tests
    
    * Adjust number of flakey tests
    
    * Try a different way of retrying tests
    
    * Add logging on retry attempt
    
    * Further adjustments to retry logic

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,6 +1,8 @@
 import * as path from "path";
 import { runTests } from "@vscode/test-electron";
-
+    
+let retries = 0;
+const maxRetries = 3;
 async function main() {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, "../../out");
@@ -16,9 +18,18 @@ async function main() {
         console.info("Success!");
         process.exit(0);
     } catch (err) {
-        console.error("Failed to run tests");
-        console.error(err);
-        process.exit(1);
+        if (retries <= maxRetries) {
+            // Provide a small wait between failure runs
+            console.info(`Error on run ${retries} with ${err}`);
+            setTimeout(() => {
+                retries++;
+                main();
+            }, 1000);
+        } else {
+            console.error("Failed to run tests");
+            console.error(err);
+            process.exit(1);
+        }
     }
 }
 
